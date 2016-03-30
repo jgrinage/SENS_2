72f00904dd7854e809fa50e0db3f79f8ba020edc
jgrinage <jmg18@rice.edu>
2016-03-30 16:11:00 -0500

    post request test works now!

--- post request test.py
+++ post request test.py
@@ -1,5 +1,6 @@
 import requests
 url = 'http://ec2-54-152-187-53.compute-1.amazonaws.com/uploadfile'
-files = {'file' : 'Bike_1.csv'}
+files = {'file': open('Bike_1.csv')}
 r = requests.post(url, files=files)
+
 
