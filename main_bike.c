db006c0adbb038193b8b468420c06e9db9e33190
YoDaMa <yoseph.maguire@gmail.com>
2016-03-29 19:22:01 -0500

    Reformatted main_bike.c
    Removed excess code and changed print statements

--- main_bike.c
+++ main_bike.c
@@ -51,8 +51,8 @@ char MMA_init_buf[] = {4, MMA8451_ADDR,  // set up Chip address
                        2, 7, 2, MMA8451_CTRL_REG1, 0x18, 3,    // put MMA in standby
                        //2, 7, 2, MMA8451_FF_MT_CFG, 0xF8, 3,    // configure interrupt to detect freefall in XYZ direction, event flag latch enabled - read FF_MT_SRC to clear flag
                        //2, 7, 2, 
-                       2, 7, 2, MMA8451_XYZ_DATA_CFG, 0x02, 3, // Configure XYZ +/- 8g (full-scale  = 8g)
-		                   2, 7, 2, MMA8451_CTRL_REG1, 0x21,3,	//initialize MMA
+						2, 7, 2, MMA8451_XYZ_DATA_CFG, 0x02, 3, // Configure XYZ +/- 8g (full-scale  = 8g)
+						2, 7, 2, MMA8451_CTRL_REG1, 0x21,3,	//initialize MMA
                        0 // EOL
                        };
 
@@ -72,71 +72,42 @@ bbI2CClose(2);
 
 gpioTerminate();
 
-if (gpioInitialise() < 0) { //initialization of GPIO library
-
-	printf("Pigpio library initialization failed\n");
-	return -1;
+while (gpioInitialise() < 0) { //initialization of GPIO library
+	static int errcounter = 0;
+	errcounter++;
+	if (errcounter>50) {
+		printf("Pigpio library initialization failed\n");
+		return -1;
+	}
+	
 }
 
-if (bbI2COpen(2,3,100000) != 0){ //start Bitbanging on with SDA = pin2, SCL = pin3
-	printf("Bit Banging initialization failed\n");
-	return -1;
+while (bbI2COpen(2,3,100000) != 0){ //start Bitbanging on with SDA = pin2, SCL = pin3
+	errcounter++;
+	if (errcounter>100) {
+		errprintf("Bit Banging initialization failed\n");
+		return -1;
+	}
 }
-
-/*** Initialize RGB ***/
-/*
-err = bbI2CZip(2,RGB_init_buf,sizeof(RGB_init_buf),NULL,0);
-while (err != 0){
-	err = bbI2CZip(2,RGB_init_buf,sizeof(RGB_init_buf),NULL,0);
-	printf ("RGB init error = %d\n", err);
-}
-printf ("RGB init pass\n");
-
-for (int i=0; i<1000;i++){};
-
-err = bbI2CZip(2,RGB_enable_buf,sizeof(RGB_enable_buf),NULL,0);
-while (err != 0){
-	err = bbI2CZip(2,RGB_enable_buf,sizeof(RGB_enable_buf),NULL,0);
-	printf ("RGB enable error = %d\n", err);
-}
-printf ("RGB enable pass\n");
-
-
-err = bbI2CZip(2,RGB_check_buf,sizeof(RGB_check_buf),RGB_check,sizeof(RGB_check) );
-while (err != 1){
-	err = bbI2CZip(2,RGB_check_buf,sizeof(RGB_check_buf),RGB_check,sizeof(RGB_check) );
-	printf("RGB check ID error = %d\n", err);
-}
-
-
-if(RGB_check[0] == 0x44){
-	printf("RGB Sensor Connected, ID = %d\n", RGB_check[0]);
-}else{
-	printf("ERROR! RGB Sensor not found\n");
-	while(bbI2CClose(2)!=0);
-	gpioTerminate();
-	return -1;
-}
-*/
 
 /*** Initialize MMA ***/
 
 err = bbI2CZip(2,MMA_init_buf,sizeof(MMA_init_buf),NULL,0);
 while (err != 0){
 	err = bbI2CZip(2,MMA_init_buf,sizeof(MMA_init_buf),NULL,0);
-	printf ("MMA init error = %d\n", err);
+	//printf ("MMA init error = %d\n", err);
 }
-printf ("MMA init pass\n");
+//printf ("MMA init pass\n");
 
 err = bbI2CZip(2,MMA_check_buf,sizeof(MMA_check_buf),MMA_check,sizeof(MMA_check) );
 while (err != 1){
 	err = bbI2CZip(2,MMA_check_buf,sizeof(MMA_check_buf),MMA_check,sizeof(MMA_check) );
-	printf("MMA check ID error = %d\n", err);
+	//printf("MMA check ID error = %d\n", err);
 }
 
 
 if(MMA_check[0] == MMA8451_WHO_AM_I_VALUE){
-	printf("MMA Sensor Connected, ID = %d\n", MMA_check[0]);
+	//printf("MMA Sensor Connected, ID = %d\n", MMA_check[0]);
 }else{
 	printf("ERROR! MMA Sensor not found\n");
 	while(bbI2CClose(2)!=0);
@@ -145,29 +116,10 @@ if(MMA_check[0] == MMA8451_WHO_AM_I_VALUE){
 }
 
 
+printf("X, Y, Z \n");
 
 for(;;){
-	/*
-	//Clear, Red, BLue, Green
-	err = bbI2CZip(2, RGB_read_buf,sizeof(RGB_read_buf),RGB_result,sizeof(RGB_result));
-
-	//Color_RGB
-	color[0]= ((RGB_result[1]<<8) + RGB_result[0]); //last two bits are empty
-	color[1]= ((RGB_result[3]<<8) + RGB_result[2]);
-	color[2]= ((RGB_result[5]<<8) + RGB_result[4]);
-	color[3]= ((RGB_result[7]<<8) + RGB_result[6]);
-
-  if (err == 8){
-	printf("CLEAR = %.2f  " ,color[0]);
-	printf("RED = %.2f  "   ,color[1]);
-	printf("BLUE = %.2f  "  ,color[2]);
-	printf("GREEN = %.2f \n\n" ,color[3]);
- }else{
- 	printf("RGB read error. error = %d \n", err);
- }
-
-*/
-	//X_MSB,X_LSB,Y_MSB,Y_LSB,Z_MSB,Z_LSB
+	
 	err = bbI2CZip(2, MMA_read_buf,sizeof(MMA_read_buf),MMA_result,sizeof(MMA_result));
 	
   //Accel XYZ
@@ -178,12 +130,12 @@ for(;;){
  	accel_g[1] = MMA_g_out(accel[1]);
  	accel_g[2] = MMA_g_out(accel[2]);
   if (err == 6){
-	printf("X = %.4f  "   ,accel_g[0]);
-	printf("Y = %.4f  "   ,accel_g[1]);
-	printf("Z = %.4f  \n" ,accel_g[2]);
+	printf("%.4f, "   ,accel_g[0]);
+	printf("%.4f, "   ,accel_g[1]);
+	printf("%.4f, \n" ,accel_g[2]);
   }
   else{
-  printf("MMA read error. error = %d \n", err);
+  //printf("MMA read error. error = %d \n", err);
   }
 	
 }
