f556d1fa7c36db1324ac452994efe1bed06fdc4d
jgrinage <jmg18@rice.edu>
2016-04-03 21:24:15 -0500

    post request should work, but it isn't. Something must be wrong with my syntax. Still figuring it out

--- post request test.py
+++ post request test.py
@@ -1,7 +1,8 @@
-import requests
-import sys
+import requests, sys
 url = 'http://ec2-54-152-187-53.compute-1.amazonaws.com/uploadfile'
-files = {'file': open(sys.argv)}
+filename = sys.argv[1]
+files = {'file': open('{}'.format(filename))}
 r = requests.post(url, files=files)
+
 
 
